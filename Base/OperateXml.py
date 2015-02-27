95ec93affed17e475a23bc787624233abc590657
284772894 <284772894@qq.com>
2015-02-27 16:51:56 +0800

    update 2015-2-12 17:00

--- Base/OperateXml.py
+++ Base/OperateXml.py
@@ -1,6 +1,11 @@
 from xml.dom import minidom
 from Base.OperateFile import base_file
-
+# web.xml 格式如下
+# <?xml version="1.0" encoding="UTF-8" ?>
+# <root>
+# 	<uu value="cc">ccc-key</uu>
+# 	<uu value="dd">dd-key</uu>
+# </root>
 def read_xml(file='c:/web.xml'):
     base_file(file).check_file()
     doc = minidom.parse(file)
