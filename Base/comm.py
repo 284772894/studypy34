4e3c6ca5918cb37289545d01642da169fac6ffdb
shikun <shikun@ibona.cn>
2015-05-06 13:08:44 +0800

    update

--- Base/comm.py
+++ Base/comm.py
@@ -3,13 +3,7 @@ class http_commom:
     list_arg = []
     request_num = []
     response_time = []
-    # count = 100 #请求总数
-    # base_url = '119.29.57.169'
-    # http_api = '/QiXun/index.php/QiXunManager/Interface/UserLogin?userName=SalesmanZy&password=1234567'
-    # method = 'get'
-    # mat = "plot" #pie bar,plot
-    # xlim = 100 # 曲线设置横x轴的上下限
-    # ylim = 3 # 曲线设置横y轴的上下限
+
     sum_03 = 0
     sum_5 = 0
     sum_1 = 0
--- sample.py
+++ sample.py
@@ -13,7 +13,6 @@ import Base.OperateXml as bo
 import Base.OperateApp as bi
 import Base.TestCase as bt
 import time
-import autoApp as au
 import matplotlib.backends.backend_tkagg
 import tkinter
 import tkinter.filedialog
@@ -23,7 +22,7 @@ br = bo.read_xml()
 http_params = {"list_arg": [], "request_num": [], "response_time": [], "sum_03": 0, "sum_5": 0, "sum_1": 0, "sum_timeout": 0}
 
 def sample_request(index=0):
-    h = http_request(base_url=br['baseurl'], http_api=br['httpapi'], method=br['method'])
+    h = http_request(base_url=br['baseurl'], http_api=br['httpapi'], method=br['method'], http_params=br['postparams'])
     res = h.request()
     http_params['request_num'].append(index+1)
     if res:
@@ -53,10 +52,10 @@ def sample_request(index=0):
                     if temp == 0:
                          http_params['sum_timeout'] += 1
 
-                sum1 = int((http_params['sum_1']/int(br["count"]))*100)
-                sum03 = int((http_params['sum_03']/int(br["count"]))*100)
-                sum5 = int((http_params['sum_5']/int(br["count"]))*100)
-                sum_timeout = int((http_params['sum_timeout']/int(br["count"]))*100)
+                sum1 = float((http_params['sum_1']/int(br["count"]))*100)
+                sum03 = float((http_params['sum_03']/int(br["count"]))*100)
+                sum5 = float((http_params['sum_5']/int(br["count"]))*100)
+                sum_timeout = float((http_params['sum_timeout']/int(br["count"]))*100)
                 list_arg = None
                 if (int(br["count"])) == (http_params['sum_1'] + http_params['sum_03'] + http_params['sum_5'] + http_params['sum_timeout']):
                     list_arg = [[u'小于300ms请求共:' + str(http_params['sum_03'])+'个', u'300-1000ms请求共:' + str(http_params['sum_1'])+'个',
@@ -71,7 +70,6 @@ def sample_request(index=0):
                 print(list_arg)
                 bm.mat_pie(list_arg, title=br["title"])
 def multi_thread():
-    starttime = time.time()
     threads = []
     for i in range(0, int(br["count"])):
         threads.append(base_thread(sample_request(index=i)))
@@ -95,40 +93,36 @@ def sample_pie():
     list_arg = [['Frogs', 'Hogs', 'Dogs', 'Logs'], ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral'], [15, 30, 45, 10]]
     bm.mat_pie(list_arg)
 
-multi_thread()
-# def sample_list(index):
-#     calculation = {
-#         'create': lambda: base_sqlite3('c:/test.db').create_sql(),
-#         'insert': lambda: base_sqlite3('c:/test.db').insert_sql(),
-#         'update': lambda: base_sqlite3('c:/test.db').update_sql(),
-#         'del': lambda: base_sqlite3('c:/test.db').del_sql(),
-#         'select': lambda: base_sqlite3('c:/test.db').select_sql(),
-#         'drop': lambda: base_sqlite3('c:/test.db').drop_table(),
-#         'read_file': lambda: base_file('c:/test.txt', 'r').read_txt_rows(),
-#         'request': lambda: sample_request(),
-#         'read_excel': lambda: be.read_excel('D:/app/PICT/result.xls'),
-#         'write_excel': lambda: be.write_excel('c:/test.xls'),
-#         'multi_thread': lambda: multi_thread(),
-#         'write_file': lambda: base_file('c:/test.txt', 'a').write_txt(["eee", "rrr", "ttt"]),
-#         'sample_matp': lambda: sample_matp(),
-#         'sample_plot': lambda: sample_plot(),
-#         'sample_pie': lambda: sample_pie(),
-#         'platform': lambda:  bp.getAllProcessInfo(),
-#         'dict_check': lambda: base_check({'a': '123', 'b': '456'}, 'a').check_index(),
-#         'list_check': lambda: base_check(['a', 'b', 'c'], '3').check_index(),
-#         'check_file': lambda: base_file().check_file(),
-#         'mkdir_file': lambda: base_file('c:/test.txt', 'w').mkdir_file(),
-#         'revome_file': lambda: base_file('c:/test.xls').remove_file(),
-#         'read_xml': lambda: bo.read_xml(),
-#         'install_app': lambda: bi.install("e/study/XX.apk"),
-#         'read_write_case': lambda: bt.read_write_case('D:/app/PICT/result.xls', 'D:/app/PICT/result1.xls'),
-#         'batch_install_app': lambda: bi.bact_install("E:\\study1\\Apps\\")
-#     }
-#     return calculation[index]()
-#
-#
-# if __name__ == "__main__":
-#     sample_list('multi_thread')
-    #adb shell 'am start -n ｛包(package)名｝/｛包名｝.{活动(activity)名称}'
-    #com.example.ffdianxun1_1
-    #com.example.ffdianxun1_1.EntryPageActivity
+def sample_list(index):
+    calculation = {
+        'create': lambda: base_sqlite3('c:/test.db').create_sql(),
+        'insert': lambda: base_sqlite3('c:/test.db').insert_sql(),
+        'update': lambda: base_sqlite3('c:/test.db').update_sql(),
+        'del': lambda: base_sqlite3('c:/test.db').del_sql(),
+        'select': lambda: base_sqlite3('c:/test.db').select_sql(),
+        'drop': lambda: base_sqlite3('c:/test.db').drop_table(),
+        'read_file': lambda: base_file('c:/test.txt', 'r').read_txt_rows(),
+        'request': lambda: sample_request(),
+        'read_excel': lambda: be.read_excel('D:/app/PICT/result.xls'),
+        'write_excel': lambda: be.write_excel('c:/test.xls'),
+        'multi_thread': lambda: multi_thread(),
+        'write_file': lambda: base_file('c:/test.txt', 'a').write_txt(["eee", "rrr", "ttt"]),
+        'sample_matp': lambda: sample_matp(),
+        'sample_plot': lambda: sample_plot(),
+        'sample_pie': lambda: sample_pie(),
+        'platform': lambda:  bp.getAllProcessInfo(),
+        'dict_check': lambda: base_check({'a': '123', 'b': '456'}, 'a').check_index(),
+        'list_check': lambda: base_check(['a', 'b', 'c'], '3').check_index(),
+        'check_file': lambda: base_file().check_file(),
+        'mkdir_file': lambda: base_file('c:/test.txt', 'w').mkdir_file(),
+        'revome_file': lambda: base_file('c:/test.xls').remove_file(),
+        'read_xml': lambda: bo.read_xml(),
+        'install_app': lambda: bi.install("e/study/XX.apk"),
+        'read_write_case': lambda: bt.read_write_case('D:/app/PICT/result.xls', 'D:/app/PICT/result1.xls'),
+        'batch_install_app': lambda: bi.bact_install("E:\\study1\\Apps\\")
+    }
+    return calculation[index]()
+
+
+if __name__ == "__main__":
+    sample_list('multi_thread')
